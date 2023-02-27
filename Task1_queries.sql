89c3caf8559d940195ca1b8d8cec51295baed4c3
gunurunithin <103179165+gunurunithin@users.noreply.github.com>
2023-02-27 09:25:45 +0530

    Add files via upload

---
+++ Task1_queries.sql
@@ -0,0 +1,28 @@
+-- create database Employee
+
+-- create table EmployeeDetails (
+-- Emp_id int not null primary key,
+-- Emp_name varchar(30) not null,
+-- Job_name varchar(30) not null,
+-- Manager_id int not null ,
+-- Hire_date varchar(30) not null ,
+-- Salary int not null ,
+-- Dep_id int not null 
+-- );
+
+-- insert into EmployeeDetails values(1,"Nithin","Manager",101,"1/01/2015",50000,201);
+-- insert into EmployeeDetails  values(2,"Manoj","Leader",102,"5/01/2015",45000,202);
+-- insert into EmployeeDetails  values(3,"Vinay","soft skills trainer",103,"10/01/2015",40000,203);
+-- insert into EmployeeDetails values(4,"Ram","Aptitude trainer",104,"15/01/2015",35000,204);
+-- insert into EmployeeDetails  values(5,"lakshman","technician",105,"20/01/2015",30000,205);
+-- insert into EmployeeDetails  values(6,"Siva teja","Coding trainer",106,"22-01-07",25000,206);
+-- insert into EmployeeDetails  values(7,"Shankar","TR",107,"25/01/2015",20000,207);
+-- insert into EmployeeDetails  values(8,"Vijay","employee-1",108,"30/01/2015",20000,208);
+-- insert into EmployeeDetails  values(9,"Mani","employee-2",109,"31/01/2015",15000,209);
+-- insert into EmployeeDetails  values(10,"vaishnav","employee-3",110,"2/02/2015",10000,210);
+
+-- select Salary from EmployeeDetails 
+
+-- select distinct Job_name from EmployeeDetails;
+
+-- update EmployeeDetails set Salary= 45000 where Emp_name ="vaishnav";
---
+++ fball_task1.sql
@@ -0,0 +1,25 @@
+-- create database Football
+
+-- create table FootballVenue(
+-- venue_id int not null primary key,
+-- venue_name varchar(40) not null,
+-- city_id int not null,
+-- capacity int not null
+-- );
+
+-- insert into FootballVenue values(1001,"Paris",111,12345);
+-- insert into FootballVenue values(1002,"India",112,45678);
+-- insert into FootballVenue values(1003,"Germany",113,56478);
+-- insert into FootballVenue values(1004,"Switz",114,12365);
+-- insert into FootballVenue values(1005,"chile",115,52136);
+-- insert into FootballVenue values(1006,"Australia",116,52136);
+-- insert into FootballVenue values(1007,"Mexico",117,12543);
+-- insert into FootballVenue values(1008,"South Africa",118,13426);
+-- insert into FootballVenue values(1009,"Argentina",119,45682);
+-- insert into FootballVenue values(1010,"Russia",120,16543);
+
+
+-- select count(venue_name)  from footballvenue;
+-- select  venue_name as Location , Capacity as Volume from footballvenue
+
+-- delete from  footballvenue where venue_name="Russia";
